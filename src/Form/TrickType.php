c5e32ec40c2dd33389e618253fa934d08a4d12c9
vcarine <you@example.com>
2022-10-18 14:55:14 +0200

    first line crud trick

---
+++ src/Form/TrickType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Trick;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TrickType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('slug')
+            ->add('description')
+            ->add('content')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('user')
+            ->add('media')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Trick::class,
+        ]);
+    }
+}
