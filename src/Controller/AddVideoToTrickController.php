1abcd1e97d747bb214c17bf373b115a7e34ade23
vcarine <you@example.com>
2022-10-25 18:20:47 +0200

    trick image&video

--- src/Controller/AddVideoToTrickController.php
+++ src/Controller/AddVideoToTrickController.php
@@ -3,22 +3,28 @@
 namespace App\Controller;
 
 use App\Entity\Image;
+use App\Entity\Video;
 use App\Form\ImageType;
+use App\Form\VideoType;
 use App\Repository\ImageRepository;
 use App\Repository\TrickRepository;
+use App\Repository\VideoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AddImageToTrickController extends AbstractController
+class AddVideoToTrickController extends AbstractController
 {
     private TrickRepository $trickRepository;
     private ImageRepository $imageRepository;
+    private VideoRepository $videoRepository;
 
-    public function __construct( TrickRepository $trickRepository, ImageRepository $imageRepository) {
+    public function __construct( TrickRepository $trickRepository, ImageRepository $imageRepository, VideoRepository $videoRepository) {
         $this->trickRepository = $trickRepository;
         $this->imageRepository = $imageRepository;
+        $this->videoRepository = $videoRepository;
 
 
     }
@@ -58,7 +64,51 @@ class AddImageToTrickController extends AbstractController
         }
         // Sinon on affiche la page et le formulaire
 
-        return $this->render('addImageTrick/index.html.twig', [
+//        return $this->renderForm('addMediaTrick/index.html.twig', [
+//            'form' => $form,
+//        ]);
+        return $this->renderForm('image/index.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/add/video/to/{slug}', name: 'app_add_video_to_trick')]
+    public function index(Request $request,string $slug): Response
+    {
+        $trick = $this->trickRepository->findOneBy(['slug' => $slug]);
+        if ($trick === null) {
+            throw new NotFoundHttpException('Impossible de trouver ce trick');
+        }
+        // On prépare le formulaire
+        $video = new Video();
+        $form = $this->createForm(VideoType::class, $image);
+        $form->handleRequest($request);
+
+        // Si le formulaire est valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile $videoFile */
+            $video= $form->get('video')->getData();
+            //Ici il faut copier l'image dans le bon répertoire
+            $originalFilename = pathinfo($videoFile->getClientOriginalName(), PATHINFO_FILENAME);
+            $newFilename = uniqid() . '.' . $videoFile->guessExtension();
+            $videoFile->move(
+                $this->getParameter('image_directory'),
+                $newFilename
+            );
+
+            $video->setLink($newFilename);
+
+
+            $video->setTrick($trick);
+
+            $this->videoRepository->save($video);
+
+            // L'image est ajoutéee et on retourne à la page home
+            return $this->redirectToRoute('app_home');
+        }
+        // Sinon on affiche la page et le formulaire
+
+        return $this->renderForm('video/_form.html.twig', [
             'form' => $form,
         ]);
     }
--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -5,13 +5,13 @@ declare(strict_types=1);
 namespace App\Form;
 
 use App\Entity\Image;
-
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Validator\Constraints\File;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use App\Form\DataTransformer\FilenameToFileTransformer;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class ImageType extends AbstractType
 {
@@ -19,19 +19,24 @@ class ImageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add('fileName', FileType::class, [
-                'label' => 'Image (.png or .jpg)',
-                'required' => false,
-                'constraints' => [
-                    new File([
-                        'maxSize' => '2048k',
-                        'mimeTypes' => [
-                            'image/png',
-                            'image/jpeg',
-                        ],
-                        'mimeTypesMessage' => 'Please upload a valid PNG or JPG file',
-                    ]),
-                ],
-                'invalid_message' => 'That is not a valid filename',
+            'label' => 'Image (.png or .jpg)',
+            'required' => false,
+            'constraints' => [
+                new File([
+                    'maxSize' => '2048k',
+                    'mimeTypes' => [
+                        'image/png',
+                        'image/jpeg',
+                    ],
+                    'mimeTypesMessage' => 'Please upload a valid PNG or JPG file',
+                ]),
+            ],
+            'invalid_message' => 'That is not a valid filename',
+        ])
+            ->add('enregistrer', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-primary'],
+                'label' => 'Enregistrer',
+                'row_attr' => ['class' => 'd-grid']
             ]);
     }
 
