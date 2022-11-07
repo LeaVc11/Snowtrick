b95ebdc64b9e47a21efc272fe59d662df8b260cf
vcarine <you@example.com>
2022-11-07 15:00:30 +0100

    first line profil1

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,10 +4,12 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
+use App\Repository\ImageRepository;
 use App\Service\AlertServiceInterface;
 use App\Service\MailService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,33 +20,33 @@ class RegisterController extends AbstractController
 {
     private EntityManagerInterface $entityManager;
 
+
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
+
     }
 
     #[Route('/register', name: 'app_register')]
     public function index(Request $request, UserPasswordHasherInterface $encoder, MailService $mailService, AlertServiceInterface $alertService): Response
     {
-        $notification = null;
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
-
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $user = $form->getData();
             $password = $encoder->hashPassword($user, $user->getPassword());
             $user->setPassword($password);
             //upload des images
-//            dd($imageData);
-            $imageFile = $form->get('image')->getData();
+
+            $imageFile = $form->get('file')->getData();
+
             $imageName = uniqid() . '.' . $imageFile->guessExtension();
             $imageFile->move(
                 $this->getParameter('image_directory'),
                 $imageName
                 );
             $user->setImage($imageName);
-            }
             $this->entityManager->persist($user);
             $this->entityManager->flush();
 
@@ -53,7 +55,7 @@ class RegisterController extends AbstractController
 
             $alertService->success('Votre inscription s\'est correctement déroulée. Vous pouvez dès à présent vous connecter à votre compte.');
 
-            return $this->redirectToRoute('app_home');
+            return $this->redirectToRoute('app_login');
         }
         return $this->render('register/index.html.twig', [
             'form' => $form->createView(),
