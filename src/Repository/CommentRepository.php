dc7c44b4af1e2eac2256372d4f32e0c0d7682627
vcarine <you@example.com>
2022-11-23 15:02:53 +0100

    update symfony insight

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -53,15 +53,4 @@ class CommentRepository extends ServiceEntityRepository
 
         ;
     }
-
-//    public function findOneBySomeField($value): ?Comment
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
-
 }
