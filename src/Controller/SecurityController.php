3c43d319ce70821fc6600a13ba9dec08febd4a75
vcarine <you@example.com>
2022-11-23 14:55:07 +0100

    update symfony insight

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -42,26 +42,19 @@ class SecurityController extends AbstractController
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
-//        $this->addFlash('success', 'Votre mot de passe a bien été mis à jour.');
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
     #[Route('/account/password', name: 'app_account_password')]
     public function index(Request $request, UserPasswordHasherInterface $encoder): Response
     {
-
         $user = $this->getUser();
         $form = $this->createForm(ChangePasswordType::class, $user);
-
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $old_pwd = $form->get('old_password')->getData();
-            /*    dd($old_pwd);*/
             if ($encoder->isPasswordValid($user, $old_pwd)) {
-                /*  die('ok)');*/
                 $new_pwd = $form->get('new_password')->getData();
-                /*dd($new_pwd);*/
                 $password=$encoder->hashPassword($user,$new_pwd);
-
                 $user->setPassword($password);
                 $this->entityManager->flush();
                 $this->addFlash('success', 'Votre mot de passe a bien été mis à jour.');
@@ -69,7 +62,6 @@ class SecurityController extends AbstractController
                 $this->addFlash('danger', 'Votre mot de passe actuel n\'est pas le bon.');
             }
         }
-
         return $this->render('account/password.html.twig', [
             'form' => $form->createView(),
         ]);
