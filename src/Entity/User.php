ea3ffb0e84e599bb1751766d28bbc12220ed6ef8
vcarine <you@example.com>
2022-11-25 12:29:07 +0100

    update

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -112,7 +112,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @see UserInterface
      */
-    public function eraseCredentials()
+    public function eraseCredentials():string
     {
     }
 
